f12f0c703cfb1c0b177bd044356f47d30deae8af
coldrain-f <intmain__@naver.com>
2024-12-19 23:08:11 +0900

    feat: Add temporary layout for household ledger data table

---
+++ app/dashboard/kakebo/monthlyLedger/columns2.tsx
@@ -0,0 +1,176 @@
+"use client";
+
+import { FilePenLineIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import * as React from "react";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Calendar } from "@/components/ui/calendar";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const FormSchema = z.object({
+  dob: z.date({
+    required_error: "A date of birth is required.",
+  }),
+});
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string;
+  transactionDate: string;
+  mainCategory: string;
+  subCategory: string;
+  incomeDescription: string;
+  amount: number;
+  note: string;
+};
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "transactionDate",
+    header: "대분류",
+  },
+  {
+    accessorKey: "mainCategory",
+    header: "목표",
+  },
+  {
+    accessorKey: "subCategory",
+    header: "소분류",
+  },
+  {
+    accessorKey: "incomeDescription",
+    header: "항목별",
+  },
+  {
+    accessorKey: "amount",
+    header: "금액",
+  },
+  {
+    accessorKey: "note",
+    header: "목표대비",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+      });
+      row.original;
+
+      function onSubmit(data: z.infer<typeof FormSchema>) {
+        alert("A");
+      }
+
+      return (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <FilePenLineIcon />
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent onPointerDownOutside={(e) => e.preventDefault()}>
+            <DialogHeader>
+              <DialogTitle className="mb-4">수입 수정</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="dob"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                          <FormLabel>날짜</FormLabel>
+                          <Popover modal={true}>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "w-[240px] pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground"
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "PPP")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                  date > new Date() ||
+                                  date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormDescription>
+                            Your date of birth is used to calculate your age.
+                          </FormDescription>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter>
+              <Button type="submit">저장</Button>
+              <DialogClose asChild>
+                <Button variant="secondary">취소</Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      );
+    },
+  },
+];
--- app/dashboard/kakebo/monthlyLedger/page.tsx
+++ app/dashboard/kakebo/monthlyLedger/page.tsx
@@ -6,24 +6,28 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { DataTable } from "./data-table";
+import { Payment, columns } from "./columns";
+import { DataTable2 } from "./data-table2";
 
-export default function MonthlyLedger() {
-  const days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
-  const weeks = [
-    ["1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7"],
-    ["1/8", "1/9", "1/10", "1/11", "1/12", "1/13", "1/14"],
-    ["1/15", "1/16", "1/17", "1/18", "1/19", "1/20", "1/21"],
-    ["1/22", "1/23", "1/24", "1/25", "1/26", "1/27", "1/28"],
-    ["1/29", "1/30", "1/31", "", "", "", ""],
-  ];
+export default async function MonthlyLedger() {
+  const data = await getData();
+
+  async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        transactionDate: "01/05(Thu)",
+        mainCategory: "상여",
+        subCategory: "아내상여",
+        incomeDescription: "",
+        amount: 1285000,
+        note: "",
+      },
+      // ...
+    ];
+  }
 
   return (
     <Card className="ml-4 mr-4">
@@ -32,84 +36,16 @@ export default function MonthlyLedger() {
         <CardDescription className="text-md">01.01 ~ 01.31</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid w-full grid-cols-4 gap-4">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-3xl">수입</CardTitle>
-              <CardDescription className="text-md">986,500円</CardDescription>
-            </CardHeader>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-3xl">저축</CardTitle>
-              <CardDescription className="text-md">80,000円</CardDescription>
-            </CardHeader>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-3xl">지출</CardTitle>
-              <CardDescription className="text-md">333,252円</CardDescription>
-            </CardHeader>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-3xl">저축률</CardTitle>
-              <CardDescription className="text-md">65.6%</CardDescription>
-            </CardHeader>
-          </Card>
-        </div>
-        <div>
-          <Card className="w-full max-w-4xl mt-4">
-            <CardHeader className="bg-violet-400 rounded-t-lg">
-              <div className="flex justify-between items-center text-white">
-                <CardTitle className="text-center flex-grow">
-                  무지출 달력
-                </CardTitle>
-                {/* <Badge variant="outline" className="bg-white text-violet-400">31일</Badge> */}
-              </div>
-            </CardHeader>
-            <CardContent className="p-0">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    {days.map((day, index) => (
-                      <TableHead
-                        key={day}
-                        className={`text-center h-10 
-                    ${index === 5 ? "text-blue-600" : ""}
-                    ${index === 6 ? "text-red-600" : ""}`}
-                      >
-                        {day}
-                      </TableHead>
-                    ))}
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {weeks.map((week, weekIndex) => (
-                    <TableRow key={weekIndex}>
-                      {week.map((date, dateIndex) => (
-                        <TableCell
-                          key={`${weekIndex}-${dateIndex}`}
-                          className={`text-center h-16 align-top pt-2
-                      ${dateIndex === 5 ? "text-blue-600" : ""}
-                      ${dateIndex === 6 ? "text-red-600" : ""}`}
-                        >
-                          {date && (
-                            <>
-                              <div>{date}</div>
-                              <div className="text-sm text-gray-500">
-                                무지출
-                              </div>
-                            </>
-                          )}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <h4 className="text-2xl font-bold mb-4">수입</h4>
+            <DataTable columns={columns} data={data} />
+          </div>
+
+          <div>
+            <h4 className="text-2xl font-bold mb-4">수입 목표</h4>
+            <DataTable2 columns={columns} data={data} />
+          </div>
         </div>
       </CardContent>
       <CardFooter></CardFooter>
